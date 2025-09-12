a30b7842743730c379be95edaba6060045440a06
CaioArgenta <127338893+CaioArgenta@users.noreply.github.com>
2025-09-12 01:41:00 +0000

    salve

---
+++ teste_rn/App.js
@@ -0,0 +1,30 @@
+import { Text, View, NavigationContainer } from 'react-native';
+
+const Tab = createBottomTabNavigation();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Home Page</Text>
+  </View>
+  )
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Settings</Text>
+  </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name= "Início" component={HomeScreen} />
+        <Tab.Screen name= "Configurações" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
